db1b67d4567bbd3f628ed69b0262e9cb970f4d39
SLICEPAY203 <aadi.sharma@slicepay.in>
2019-07-08 21:54:45 +0530

    changes in go module

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/bnkamalesh/webgo/middleware"
 
 	"github.com/aadi756/cardflow/app/api/rest"
 	"github.com/bnkamalesh/webgo"
+	_ "rsc.io/quote"
 )
+
 
 func main() {
 	cfg := webgo.Config{
 		Host:         "",
-		Port:         "8000",
+		Port:         "8080",
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 60 * time.Second,
 	}
+	fmt.Println(quote.Hello())
 	router := webgo.NewRouter(&cfg, rest.GetRoutes())
 	router.Use(middleware.AccessLog)
 	router.Start()
--- app/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/aadi756/cardflow/app
-
-go 1.12
-
-require github.com/bnkamalesh/webgo v2.5.0+incompatible
